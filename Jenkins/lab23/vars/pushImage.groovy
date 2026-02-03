76ba49d39a1114aa8829e9721cad89d52cba775b
Rawan Raslan <r.abdelrahman3332@su.edu.eg>
2026-02-04 00:48:31 +0200

    Update pushImage.groovy

--- Jenkins/lab23/vars/pushImage.groovy
+++ Jenkins/lab23/vars/pushImage.groovy
@@ -1,4 +1,8 @@
 def call(String image) {
-    sh "docker push ${image}"
+    withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', 
+                                     usernameVariable: 'DOCKER_USER', 
+                                     passwordVariable: 'DOCKER_PASS')]) {
+        sh "docker login -u $DOCKER_USER -p $DOCKER_PASS"
+        sh "docker push ${image}"
+    }
 }
-
