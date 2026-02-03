ae3be30dea86da78f200f3865996a42fdf0812e4
Rawan Raslan <r.abdelrahman3332@su.edu.eg>
2026-02-03 23:47:50 +0200

    Update buildImage.groovy

--- Jenkins/lab23/vars/buildImage.groovy
+++ Jenkins/lab23/vars/buildImage.groovy
@@ -1,5 +1,5 @@
 def call(image) {
     stage('BuildImage') {
-        sh "docker build -t ${image} -f docker-labs/iVolve-Tasks/Jenkins/lab23/Dockerfile docker-labs/iVolve-Tasks/Jenkins/lab23"
+        sh "docker build -t ${image} -f Jenkins/lab23/Dockerfile docker-labs/iVolve-Tasks/Jenkins/lab23"
     }
 }
